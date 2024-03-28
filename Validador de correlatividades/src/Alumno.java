a1499a9c462fae47d2f9e63af035ab67b468f8d2
Facundo Mechulam <fmechulam@frba.utn.edu.ar>
2024-03-28 20:48:18 -0300

    empiezo el boceto del ejercicio

---
+++ Validador de correlatividades/src/Alumno.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Alumno {
+
+    private List<Materia> materiasAprobadas;
+
+    public List<Materia> getMateriasAprobadas() {
+        return materiasAprobadas;
+    }
+}
--- Validador de correlatividades/src/Main.java
+++ Validador de correlatividades/src/Main.java
@@ -1,5 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+class Materia {
+    private String nombre;
+    public Materia(String nombre) {
+        this.nombre = nombre;
+        correlativas = new ArrayList<>();
+    }
+
+    public void agregarCorrelativa(Materia materia) {
+        correlativas.add(materia);
+    }
+
+    public List<Materia> getCorrelativas() {
+        return correlativas;
+    }
+}
+
+class Inscripcion {
+    private Alumno alumno;
+    private List<Materia> materias;
+
+    public Inscripcion(Alumno alumno, List<Materia> materias) {
+        this.alumno = alumno;
+        this.materias = materias;
+    }
+
+    public boolean aprobada() {
+        for (Materia materia : materias) {
+            for (Materia correlativa : materia.getCorrelativas()) {
+                if (!alumno.aprobada(correlativa)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Alumno alumno = new Alumno("Juan");
+
+        Materia estructurasDatos = new Materia("Algoritmos y Estructuras de Datos");
+        Materia paradigmasProgramacion = new Materia("Paradigmas de Programación");
+        Materia disenoSistemas = new Materia("Diseño de Sistemas");
+
+        paradigmasProgramacion.agregarCorrelativa(estructurasDatos);
+        disenoSistemas.agregarCorrelativa(paradigmasProgramacion);
+
+        List<Materia> materiasInscripcion = new ArrayList<>();
+        materiasInscripcion.add(disenoSistemas);
+
+        Inscripcion inscripcion = new Inscripcion(alumno, materiasInscripcion);
+
+        if (inscripcion.aprobada()) {
+            System.out.println("Inscripción aprobada.");
+        } else {
+            System.out.println("Inscripción rechazada.");
+        }
+    }
 }
